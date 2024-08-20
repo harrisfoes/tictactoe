0088d0a4ec3bdd632a0d2eb4225ae30f22910028
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-08-20 14:25:52 +0900

    more setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [playerTurn, setPlayerTurn] = useState("x");
   return (
     <>
+      <div className="text-3xl font-bold">
+        <p>Harris:</p>
+        <p>Tic Tac Toe project</p>
+        <p>The current player turn is {playerTurn}</p>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className="border">X</button>
+        <button>O</button>
+        <button>X</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
